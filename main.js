6908256fc93e569b240f289e8bb0f11904529a40
jeremy-mazzocco <jeremyozzg@gmail.com>
2023-05-10 13:15:08 +0100

    add bombs and red color

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const selectContainer = document.querySelector('.container');
 const selectContaPunti = document.getElementById('punteggio');
+const arrayBombe = [];
 
 
 // Aggiungo funzionalita' al click del Bottone
@@ -26,20 +27,29 @@ selectButton.addEventListener('click',
             numeroDiCelle = 49;
         }
 
+        // CREA LE BOMBE
+        // metti 16 numeri casuali in una array in base al numero di celle
+
+        while (arrayBombe.length <= 15) {
+            const numeriDelleBombe = randomNumber(1, numeroDiCelle);
+            if (!arrayBombe.includes(numeriDelleBombe)) {
+                arrayBombe.push(numeriDelleBombe);
+            }
+        }
+        console.log(arrayBombe);
+
         // Cicla per 100 volte le seguenti operazioni
         for (let i = 1; i <= numeroDiCelle; i++) {
 
             // Crea un elemento
             const createSquare = document.createElement('div');
-
             // dagli una classe
             createSquare.classList.add('square');
-
             // appendilo al container
             selectContainer.append(createSquare);
-
             // scrivi il numero all'interno dell'elemento
-            createSquare.innerHTML = i;
+            let numeroDelleCelle = i
+            createSquare.innerHTML = numeroDelleCelle;
 
             // BONUS: in base al livello selezionato: cambia la classe
             if (livelloSelezionato === "Easy") {
@@ -50,22 +60,26 @@ selectButton.addEventListener('click',
                 createSquare.classList.add('square-hard');
             }
 
-
-
-            // colora la cella al click
+            // Al Click
             createSquare.addEventListener('click',
                 function () {
+                    // colora la cella al click
                     createSquare.style.backgroundColor = 'blue';
 
                     // aggiungi +1 al puntggio
                     punteggio += 1;
-
                     // scrivi punti su HTML
                     selectContaPunti.innerHTML = `Il tuo puteggio: ${punteggio}`;
 
-
-
-                });
+                    // controlla che il numero nell'arrayBombe corrisponda con il numero della cella
+                    for (let i = 0; i <= arrayBombe.length; i++) {
+                        if (arrayBombe[i] === numeroDelleCelle) {
+                            console.log("bomb");
+                            createSquare.style.backgroundColor = 'red';
+                        }
+                    }
+                }
+            );
         }
     }
 );
@@ -74,7 +88,11 @@ selectButton.addEventListener('click',
 
 
 //  MY FUNCTIONS
+function randomNumber(min, max) {
+    const random = Math.floor(Math.random() * (max - min + 1) + min);
+    return random
 
+}
 
 
 
